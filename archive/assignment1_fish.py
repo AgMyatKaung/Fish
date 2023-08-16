079f6b244d7d590675a3c38c742c9e1636d80f33
Aung Myat Kaung <aungmyatkaung.samk@gmail.com>
2023-08-16 20:12:44 +0630

    fish

--- archive/assignment1_fish.py
+++ archive/assignment1_fish.py
@@ -1,9 +1,9 @@
+
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
 
 # Load the data
 data = pd.read_csv('AI/archive/Fish.csv')
@@ -36,11 +36,90 @@ r2 = r2_score(y_test, y_pred)
 print("Mean Squared Error:", mse)
 print("R-squared:", r2)
 
+
+# In[7]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load data
+data = pd.read_csv('AI/archive/Fish.csv')
+columns = ["Species", "Weight", "Length1", "Length2", "Length3", "Height", "Width"]
+
+df = pd.DataFrame(data, columns=columns)
+
+
+# In[8]:
+
+
+# Convert species to numerical labels
+df['Species'] = pd.Categorical(df['Species']).codes
+
+
+# In[9]:
+
+
+# Prepare data
+X = df.drop("Weight", axis=1)
+y = df["Weight"]
+
+
+# In[10]:
+
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# In[11]:
+
+
 # Create polynomial features
 degree = 2  # Degree of the polynomial
 poly = PolynomialFeatures(degree)
 X_train_poly = poly.fit_transform(X_train)
 X_test_poly = poly.transform(X_test)
+
+
+# In[12]:
+
+
+# Train polynomial regression model
+model = LinearRegression()
+model.fit(X_train_poly, y_train)
+
+
+# In[13]:
+
+
+# Make predictions
+y_pred = model.predict(X_test_poly)
+
+
+# In[14]:
+
+
+# Evaluate model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+
+# In[15]:
+
+
+
+# Print evaluation results
+print("Mean Squared Error:", mse)
+print("R-squared:", r2)
+
+
+# In[21]:
+
 
 # Select a Roach instance from the test set for prediction (you can adjust the index)
 roach_instance = X_test.iloc[[0]]
@@ -53,6 +132,10 @@ roach_weight_prediction = model.predict(roach_instance_poly)
 
 print("Predicted weight for Roach:", roach_weight_prediction)
 
+
+# In[26]:
+
+
 import matplotlib.pyplot as plt
 
 # Plot scatter plot
@@ -64,3 +147,10 @@ plt.ylabel('Predicted Weight')
 plt.title('Actual vs. Predicted Weight')
 plt.legend()
 plt.show()
+
+
+# In[ ]:
+
+
+
+
